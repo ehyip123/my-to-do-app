e2afeeb423f5e3d1bfa0bffb6a0709bedbf50f2c
ehyip123 <graceyip@live.com.sg>
2023-02-26 11:41:51 +0800

    added functionality

--- functions.py
+++ functions.py
@@ -7,3 +7,12 @@ def get_todos(filepath = 'todolist.txt'):
 def write_file(todos_arg, filepath='todolist.txt'):
     with open(filepath, 'w') as f:
         f.writelines(todos_arg)
+
+def get_completed(filepath = 'completed.txt'):
+    with open(filepath, 'r') as f:
+        completed = f.readlines()
+        return completed
+
+def write_complete(complete_arg, filepath = 'completed.txt'):
+    with open(filepath,'w' ) as f:
+        f.writelines(complete_arg)
--- webtodo.py
+++ webtodo.py
@@ -4,8 +4,10 @@
 # In[1]:
 import streamlit as st
 import functions
+import time
 
 todos = functions.get_todos()
+complete = functions.get_completed()
 
 def add_todo():
     newtodo = st.session_state["new_todo"] + "\n"
@@ -19,13 +21,19 @@ st.write('Keep track of your tasks')
 #giveeachtodoauniquekeybyusingindex
 for i, todo in enumerate(todos):
     checkbox = st.checkbox(todo,key={i})
-
+    #if checkbox = true
     if checkbox:
-        todos.pop(i)
+        complete.append(todos.pop(i))
+        functions.write_complete(complete)
         functions.write_file(todos)
         del st.session_state[{i}]
         st.experimental_rerun()
 
 st.text_input(label="", placeholder='Add new task',
               on_change=add_todo,key="new_todo")
+
+st.write('Completed items:')
+completed = st.empty()
+for todo in complete:
+    st.write('- ' + time.strftime('%d-%m-%Y')+ ' '+ todo)
 
